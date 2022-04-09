990620dabe09620d588ce0c6d626c15bb80428e5
vivek gupta <vivekgupta6534294@gmail.com>
2022-04-09 23:34:09 +0530

    Web Scrapping

--- leaderboard.js
+++ leaderboard.js
@@ -1,7 +1,9 @@
 const request=require('request');
 const jsdom=require("jsdom");
 const {JSDOM}=jsdom;
+let fs=require("fs");
 let leaderBoard=[];
+let counter=0;
 const link="https://www.espncricinfo.com/series/ipl-2021-1249214/match-results";
 request(link,cb);
 function cb(error,response,html){
@@ -17,6 +19,7 @@ function cb(error,response,html){
             let completeLink ="https://www.espncricinfo.com"+link;
             // console.log(completeLink);
             request(completeLink,scoreCard);
+            counter++;//no of request in node api
         }
     }
 }
@@ -32,21 +35,29 @@ function scoreCard(error, response, html){
             let cell=batsmenRow[i].querySelectorAll("td");
             if(cell.length==8){
                 let name=cell[0].textContent;
-                let runs=cell[0].textContent;
+                let runs=cell[2].textContent;
                 let balls=cell[3].textContent;
                 let fours=cell[5].textContent;
                 let sixes=cell[6].textContent;
                 // console.log("Name: ", name, "Runs", runs, "Balls", balls,"Four",fours,"Sixes",sixes);
-               // processPlayer(name,runs,balls,fours,sixes);
+               processPlayer(name,runs,balls,fours,sixes);
             }
         }
+        counter--;
+        if(counter==0){
+            console.log(leaderBoard);
+            let data=JSON.stringify(leaderBoard)
+            fs.writeFileSync("BatsmenStat.json",data);
+        }
+        // console.log(leaderBoard);
     }
 }
-    processPlayer('Rohit','15','4','2','4');
-    processPlayer('Virat','50','20','4','3')
-    processPlayer('Rohit','40','20','1','2');
-    console.log(leaderBoard);
-function processPlayer(name, runs, balls, fours, sixes){
+    // processPlayer('Rohit','15','4','2','4');
+    // processPlayer('Virat','50','20','4','3')
+    // processPlayer('Rohit','40','20','1','2');
+    // console.log(leaderBoard);
+
+function processPlayer(name,runs, balls, fours, sixes){
     
     runs=Number(runs);
     balls=Number(balls);
@@ -57,6 +68,7 @@ function processPlayer(name, runs, balls, fours, sixes){
         let playerObj=leaderBoard[i];
         if(playerObj.Name==name){
             playerObj.Runs+=runs;
+            playerObj.Innings+=1;
             playerObj.Balls+=balls;
             playerObj.Fours+=fours;
             playerObj.Sixes+=sixes;
@@ -66,6 +78,7 @@ function processPlayer(name, runs, balls, fours, sixes){
         // if not exist
         let obj={
             Name:name,
+            Innings:1,
             Runs:runs,
             Balls:balls,
             Fours:fours,
@@ -74,3 +87,5 @@ function processPlayer(name, runs, balls, fours, sixes){
         leaderBoard.push(obj);
     
 }
+// console.log("Last position",leaderBoard);
+console.log("This Code is Working ");
